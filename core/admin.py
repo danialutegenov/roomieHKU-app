94e56142d4a6f03ee654a3849745fc5edfcbb20a
xin-2005 <u3638913@connect.hku.hk>
2026-04-05 19:58:51 +0800

    Implement CustomUserAdmin for User model
    Added CustomUserAdmin to manage additional user fields.

--- core/admin.py
+++ core/admin.py
@@ -3,6 +3,14 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import User, Post, Comment, Like, SavedListing
 
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('bio', 'phone_number', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('bio', 'phone_number', 'profile_photo')}),
+    )
+
 admin.site.register(User, UserAdmin)
 admin.site.register(Post)
 admin.site.register(Comment)
