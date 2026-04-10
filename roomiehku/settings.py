ab9984a05be6e26dfc2ce93e708a13d03da840a2
Danial Utegenov <danial.utegenov@gmail.com>
2026-04-10 14:46:07 +0800

    Revert "Implement complete Django SSR backend MVP flows and tests"
    This reverts commit ba31cc71356b93c11cc23f61cdc43e99fce739bd.

--- roomiehku/settings.py
+++ roomiehku/settings.py
@@ -70,7 +70,3 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-LOGIN_URL = "/auth/login/"
-LOGIN_REDIRECT_URL = "/app/listings/"
-LOGOUT_REDIRECT_URL = "/app/listings/"
